1ba0b9a8cfd74f9f2a7184ed366034c1124f6f92
Chen9818 <88109684+Chen9818@users.noreply.github.com>
2022-03-12 00:52:55 +0800

    fix product page

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,8 +1,90 @@
 <template>
-  <NavbarView></NavbarView>
-  <div class="container">
+  <div class="product w-100" style="background:#ccc">
+    <NavbarView></NavbarView>
     <Loading :active="isLoading"></Loading>
-    <img :src="product.imageUrl" alt="">
+    <div class="container" style="padding-top:13rem">
+      <div class="productShow">
+        <div class="img">
+          <img :src="product.imageUrl" :alt="product.title">
+        </div>
+        <div class="txt">
+          <h1>{{product.title}}</h1>
+          <h2>{{product.description}}</h2>
+          <p>尺寸:{{product.content}}</p>
+          <p>原價:{{product.origin_price}}</p>
+          <p>特價:{{product.price}}</p>
+          <div class="addToCart d-flex">
+              <select
+                name="productNum"
+                class="form-select"
+                style="width: 100%"
+              >
+                <option>
+                  1
+                </option>
+              </select>
+              <button type="button" class="btn btn-primary">Primary</button>
+          </div>
+        </div>
+      </div>
+      <div class="other">
+        <div class="title">
+          <h2>其他</h2>
+        </div>
+
+        <div class="container">
+          <div class="row">
+            <div class="products col-12 col-md-4 d-flex flex-wrap" v-for="item in products" :key="item.id">
+              <div class="d" v-if="product.category === item.category">
+                <div class="card mx-auto my-3" style="width: 100%;">
+                  <img
+                    :src="item.imageUrl"
+                    class="card-img-top"
+                    :alt="item.title"
+                  />
+                  <div class="card-body">
+                    <div class="d-flex">
+                      <h4 class="card-title">
+                        {{ item.title }}
+                      <span class="badge bg-base ms-2">{{ item.category }}</span>
+                      </h4>
+                    </div>
+                      <div class="card-text d-flex justify-content-between" style="font-size:1.2rem">
+                      <p class="text-decoration-line-through">
+                        原:NT{{ item.origin_price }}
+                      </p>
+                      <p style="color:#ff0000">
+                        特:NT{{ item.price }}
+                      </p>
+                      </div>
+                    <div class="d-flex justify-content-between">
+                      <button
+                      type="button"
+                      class="btn btn-outline-secondary"
+                      >
+                        <i
+                        class="fas fa-spinner fa-pulse"
+                        ></i>
+                        查看更多
+                      </button>
+                      <button
+                      type="button"
+                      class="btn btn-outline-danger"
+                      >
+                        <i
+                        class="fas fa-spinner fa-pulse"
+                      ></i>加入購物車</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,7 +95,8 @@ export default {
   data () {
     return {
       product: {},
-      isLoading: false
+      isLoading: false,
+      products: []
     }
   },
   components: {
@@ -33,10 +116,72 @@ export default {
         .catch((err) => {
           alert(err.data.message)
         })
+    },
+    getProducts () {
+      this.isLoading = true
+      const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/products/all`
+      this.$http
+        .get(url)
+        .then((response) => {
+          // const { products, pagination } = response.data
+          this.products = response.data.products
+          // this.pagination = pagination
+          this.filterTitle = '全部'
+          this.isLoading = false
+        })
+        .catch((err) => {
+          alert(err.data.message)
+        })
     }
   },
   mounted () {
     this.getProduct()
+    this.getProducts()
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import "@/assets/base/all.scss";
+  .container .productShow {
+    display: flex;
+    justify-content: center;
+    box-sizing: border-box;
+    .img{
+      width: 60%;
+      height:30rem;
+      img{
+        width: 100%;
+        height: 100%;
+        background-position: center center;
+        background-size: cover;
+      }
+    }
+    .txt{
+      // height: 15rem;
+      width: 40%;
+      background: #fff;
+      // margin-left:-33px ;
+      transform: translateX(-33px);
+      margin: auto;
+    }
+  }
+
+  @media (max-width:780px){
+  .container .productShow {
+    display: block;
+    .img{
+      width: 100%;
+      height:30rem;
+    }
+    .txt{
+      // height: 15rem;
+      width: 100%;
+      background: #fff;
+      // margin-left:-33px ;
+      transform: translateX(0);
+      margin: auto;
+    }
+  }
+}
+</style>
