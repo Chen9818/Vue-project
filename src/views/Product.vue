5c140ec4b358f352837da57bf519f4fa7545ddaf
Chen9818 <88109684+Chen9818@users.noreply.github.com>
2022-03-13 00:53:30 +0800

    product page

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -14,28 +14,35 @@
           <p>原價:{{product.origin_price}}</p>
           <p>特價:{{product.price}}</p>
           <div class="addToCart d-flex">
-              <select
+              <!-- <select
                 name="productNum"
                 class="form-select"
                 style="width: 100%"
+                v-model="product.qty"
+                :disabled="loadingStatus.loadingItem === product.id"
               >
-                <option>
-                  1
+                <option  :value="num" v-for="num in 8" :key="num">
+                  {{num}}
                 </option>
-              </select>
-              <button type="button" class="btn btn-primary">Primary</button>
+              </select> -->
+              <button
+              type="button"
+              @click="addToCart(product.id)"
+              class="btn btn-primary">加入購物車</button>
+              <button
+              type="button"
+              class="btn btn-primary">已達購買上限</button>
           </div>
         </div>
       </div>
       <div class="other">
         <div class="title">
-          <h2>其他</h2>
+          <h2>相關商品{{cart}}</h2>
         </div>
 
         <div class="container">
           <div class="row">
-            <div class="products col-12 col-md-4 d-flex flex-wrap" v-for="item in products" :key="item.id">
-              <div class="d" v-if="product.category === item.category">
+            <div class="products col-12 col-md-4" v-for="item in sameProduct" :key="item.id" >
                 <div class="card mx-auto my-3" style="width: 100%;">
                   <img
                     :src="item.imageUrl"
@@ -61,23 +68,28 @@
                       <button
                       type="button"
                       class="btn btn-outline-secondary"
+                      @click="getToProduct(item.id)"
+                      :disabled="loadingStatus.loadingItem === item.id || !item.is_enabled"
                       >
                         <i
                         class="fas fa-spinner fa-pulse"
+                        v-if="loadingStatus.loadingItem === item.id"
                         ></i>
                         查看更多
                       </button>
                       <button
                       type="button"
                       class="btn btn-outline-danger"
+                      @click="addToCart(item.id)"
+                      :disabled="loadingStatus.loadingItem === item.id || !item.is_enabled"
                       >
                         <i
                         class="fas fa-spinner fa-pulse"
+                        v-if="loadingStatus.loadingItem === item.id"
                       ></i>加入購物車</button>
                     </div>
                   </div>
                 </div>
-              </div>
 
             </div>
           </div>
@@ -90,13 +102,21 @@
 
 <script>
 import NavbarView from '@/components/NavbarView.vue'
+import emitter from '../utility/emitter'
 
 export default {
   data () {
     return {
-      product: {},
+      product: {
+        qty: ''
+      },
       isLoading: false,
-      products: []
+      products: [],
+      sameProduct: [],
+      loadingStatus: {
+        loadingItem: ''
+      },
+      cart: []
     }
   },
   components: {
@@ -111,11 +131,17 @@ export default {
         .get(url)
         .then((response) => {
           this.product = response.data.product
+          this.getProducts()
+
           this.isLoading = false
         })
         .catch((err) => {
           alert(err.data.message)
         })
+    },
+    getToProduct (id) {
+      // this.$router.push('/products')
+      this.$router.push(`/product/${id}`)
     },
     getProducts () {
       this.isLoading = true
@@ -123,47 +149,112 @@ export default {
       this.$http
         .get(url)
         .then((response) => {
-          // const { products, pagination } = response.data
           this.products = response.data.products
-          // this.pagination = pagination
-          this.filterTitle = '全部'
+          this.sameProduct = this.products.filter((item) => {
+            return item.category === this.product.category && item.title !== this.product.title
+          })
           this.isLoading = false
         })
         .catch((err) => {
           alert(err.data.message)
         })
+    },
+    addToCart (id, qty = 1) {
+      const url = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/cart`
+      this.loadingStatus.loadingItem = id
+      const cart = {
+        product_id: id,
+        qty: qty
+      }
+      this.$http.post(url, { data: cart }).then((response) => {
+        emitter.emit('cart')
+        // alert(response.data.message)
+        // this.getCart(id)
+        this.$router.push('/carts')
+        this.loadingStatus.loadingItem = ''
+      }).catch((err) => {
+        alert(err.data.message)
+      })
     }
+    // getCart (id) {
+    //   const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/cart`
+    //   this.isLoading = true
+    //   this.$http
+    //     .get(url)
+    //     .then((response) => {
+    //       this.cart = response.data.data.carts.product.filter(item => item.id === id)
+    //       this.isLoading = false
+    //     })
+    //     .catch((err) => {
+    //       alert(err.data.message)
+    //     })
+    // }
+    // updateCart (data) {
+    //   this.loadingStatus.loadingItem = data.id
+    //   const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/cart/${data.id}`
+    //   const cart = {
+    //     product_id: data.id,
+    //     qty: data.qty
+    //   }
+    //   this.$http
+    //     .put(url, { data: cart })
+    //     .then((response) => {
+    //       alert(response.data.message)
+    //       this.loadingStatus.loadingItem = ''
+    //       this.getCart()
+    //     })
+    //     .catch((err) => {
+    //       alert(err.data.message)
+    //       this.loadingStatus.loadingItem = ''
+    //     })
+    // }
+
   },
   mounted () {
     this.getProduct()
-    this.getProducts()
   }
 }
 </script>
 
 <style lang="scss" scoped>
 @import "@/assets/base/all.scss";
-  .container .productShow {
+  .container{
+    .productShow {
     display: flex;
     justify-content: center;
     box-sizing: border-box;
-    .img{
-      width: 60%;
-      height:30rem;
-      img{
-        width: 100%;
-        height: 100%;
-        background-position: center center;
-        background-size: cover;
+      .img{
+        width: 60%;
+        height:30rem;
+        img{
+          width: 100%;
+          height: 100%;
+          background-position: center center;
+          background-size: cover;
+        }
+      }
+      .txt{
+        // height: 15rem;
+        width: 40%;
+        background: #fff;
+        // margin-left:-33px ;
+        transform: translateX(-33px);
+        margin: auto;
       }
     }
-    .txt{
-      // height: 15rem;
-      width: 40%;
-      background: #fff;
-      // margin-left:-33px ;
-      transform: translateX(-33px);
-      margin: auto;
+    .other .card {
+    opacity: 0.8;
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+      &:hover {
+        opacity: 1;
+        img {
+          border: 10px solid #fff;
+        }
+      }
     }
   }
 
